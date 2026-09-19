d05cdd2d2a085773b893ff04fb3e26f4d3592699
Medatur76 <medaturd76@gmail.com>
2026-09-19 18:25:21 -0500

    oml it works jesus christ

--- run.sh
+++ run.sh
@@ -1,13 +1,9 @@
 [ ! -d "bin" ] && mkdir bin
 
-as main.S -o bin/main.o -g
+as main.S -o bin/main.o
 
 ld bin/main.o -o bin/main
-
-#gcc main.c -o bin/main -g -lm
 
 chmod +x ./bin/main
 
 ./bin/main
-
-echo $?
