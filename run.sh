733824f0ef8c1eafd9d9bcdbb4880b46e824da8a
Medatur76 <medaturd76@gmail.com>
2026-09-18 20:31:25 -0500

    back to asm :)

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 [ ! -d "bin" ] && mkdir bin
 
-#as main.S -o bin/main.o -g
+as main.S -o bin/main.o -g
 
-#ld bin/main.o -o bin/main
+ld bin/main.o -o bin/main
 
-gcc main.c -o bin/main -g -lm
+#gcc main.c -o bin/main -g -lm
 
 chmod +x ./bin/main
 
