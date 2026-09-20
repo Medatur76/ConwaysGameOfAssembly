4812e3227a2d640ac6d19645c20e8ec11506e438
Medatur76 <medaturd76@gmail.com>
2026-09-19 23:46:53 -0500

    Horrible but cursor plus instructions

--- main.c
+++ main.c
@@ -5,6 +5,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <stdbool.h>
+
+#define min(a, b) (a) < (b) ? (a) : (b)
+#define max(a, b) (a) > (b) ? (a) : (b)
 
 struct termios orig_termios;
 
@@ -51,8 +55,22 @@ struct pollfd fds = {STDIN_FILENO, POLLIN, 0};
 
 struct point {int x; int y;};
 
-void draw(struct point points[], unsigned long nPoints) {
+void drawPoints(struct point points[], unsigned long nPoints) {
     for (unsigned long i = 0; i < nPoints; i++) procBuff[(points[i].y*w + points[i].x) / 8] |= 1 << ((points[i].y*w + points[i].x) % 8);
+}
+
+void drawBox() {
+    write(1, "┌", 3);
+    for (int i = 0; i < w; i++) write(1, "─", 3);
+    write(1, "┐\r\n", 5);
+    for (int i = 0; i < ceil((double) h / 2); i++) {
+        write(1, "│", 3);
+        for (int j = 0; j < w; j++) write(1, " ", 1);
+        write(1, "│\r\n", 5);
+    }
+    write(1, "└", 3);
+    for (int i = 0; i < w; i++) write(1, "─", 3);
+    write(1, "┘\x1b[3;2f", 9);
 }
 
 int main() {
@@ -76,24 +94,23 @@ int main() {
         [9] = {.x = w-3, .y = 3}
     };
 
-    draw(defaultState, sizeof(defaultState) / sizeof(defaultState[0]));
+    drawPoints(defaultState, sizeof(defaultState) / sizeof(defaultState[0]));
 
     enable_raw_mode();
 
-    write(1, "\x1b[?1049h\x1b[3J\x1b[2J\x1b[H\x1b[?25lKiernan's Game of C\r\n┌", 49);
+    write(1, "\x1b[?1049h\x1b[3J\x1b[2J\x1b[H\x1b[?25lMedatur76's Game of C\r\n", 48);
 
-    for (int i = 0; i < w; i++) write(1, "─", 3);
-    write(1, "┐\r\n", 5);
-    for (int i = 0; i < ceil((double) h / 2); i++) {
-        write(1, "│", 3);
-        for (int j = 0; j < w; j++) write(1, " ", 1);
-        write(1, "│\r\n", 5);
-    }
-    write(1, "└", 3);
-    for (int i = 0; i < w; i++) write(1, "─", 3);
-    write(1, "┘\x1b[3;2f", 9);
+    drawBox();
+
+    bool paused = true, overwrite = true;
+    struct point cursor = {0,0};
+
+    write(1, "\x1b[2;0f", 6);
+
     while (1) {
         memcpy(displayBuff, procBuff, bSize);
+        drawBox();
+        if (paused && overwrite) displayBuff[(cursor.y*w + cursor.x) / 8] ^= 1 << ((cursor.y*w + cursor.x) % 8);
         for (int y = 0; y < (h / 2); y++) {
             for (int x = 0; x < w; x++) {
                 int i = y*2*w+x;
@@ -107,6 +124,7 @@ int main() {
                 } else {
                     write(1, " ", 1);
                 }
+                if (paused) continue;
                 int n = getNeighbors(x, y*2);
                 if ((a & 2 && (n < 2 || n > 3)) || (!(a & 2) && n == 3)) procBuff[i / 8] ^= (1 << (i % 8));
                 if ((y*2+1) < h) {
@@ -116,14 +134,44 @@ int main() {
             }
             write(1, "\x1b[1B\x1b[2G", 8);
         }
-        write(1, "\x1b[3;2f", 6);
 
-        if(poll(&fds, 1, 150) && (fds.revents & POLLIN)) {
+        if (paused || (poll(&fds, 1, 150) && (fds.revents & POLLIN))) {
             //Will need this to be dynamic OR as big as the biggest input I wish to process
             char in;
             read(STDIN_FILENO, &in, 1);
-            if (in == 0x0A || in == 0x0D) break;
+            overwrite = true;
+            if (in == 0x20) {
+                if (paused ^= 1) {
+                    write(1, "\x1b[0J\r\n\nUse the arrow keys to move the cursor\r\nPress enter to flip the selected cell\r\nPress space to resume the game", 115);
+                } else {
+                    write(1, "\x1b[0J\r\n\nPress space to pause the game\r\nPress enter to exit", 57);
+                }
+            } else if (in == 0x1B && read(STDIN_FILENO, &in, 1) && in == 0x5B && read(STDIN_FILENO, &in, 1)) {
+                switch (in) {
+                    case 0x41:
+                        cursor.y = max(cursor.y - 1, 0);
+                        break;
+                    case 0x42:
+                        cursor.y = min(cursor.y + 1, h - 1);
+                        break;
+                    case 0x43:
+                        cursor.x = min(cursor.x + 1, w - 1);
+                        break;
+                    case 0x44:
+                        cursor.x = max(cursor.x - 1, 0);
+                        break;
+                    default:
+                        break;
+                }
+            } else if (in == 0x0A || in == 0x0D) {
+                if (paused) {
+                    procBuff[(cursor.y*w + cursor.x) / 8] ^= 1 << ((cursor.y*w + cursor.x) % 8);
+                    overwrite = false;
+                } else break;
+            }
         }
+
+        write(1, "\x1b[2;0f", 6);
     }
 
     write(1, "\x1b[3J\x1b[2J\x1b[H\x1b[?25h\x1b[?1049l", 25);
